1d95f6cae9faba30a3b2ba38b457c4d8044e2659
Ruslan <stadnikruslan168@gmail.com>
2026-06-11 16:35:27 +0300

    Added registration models to models.users.py

--- Online-cinema-app/db/session_postgresql.py
+++ Online-cinema-app/db/session_postgresql.py
@@ -4,10 +4,11 @@ from typing import AsyncGenerator
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine, AsyncSession
 from sqlalchemy.orm import declarative_base
 
-
-POSTGRESQL_DATABASE_URL = (f"postgresql+asyncpg://{os.getenv("POSTGRES_USERNAME")}:"
-                         f"{os.getenv("POSTGRES_PASSWORD")}@{os.getenv("POSTGRES_HOST")}:"
-                         f"{os.getenv("POSTGRES_DB_PORT")}/{os.getenv("POSTGRES_DB")}")
+POSTGRESQL_DATABASE_URL = (
+    f"postgresql+asyncpg://{os.getenv("POSTGRES_USERNAME")}:"
+    f"{os.getenv("POSTGRES_PASSWORD")}@{os.getenv("POSTGRES_HOST")}:"
+    f"{os.getenv("POSTGRES_DB_PORT")}/{os.getenv("POSTGRES_DB")}"
+)
 
 postgresql_engine = create_async_engine(POSTGRESQL_DATABASE_URL, echo=False)
 
@@ -16,10 +17,11 @@ AsyncPostgresqlSessionLocal = async_sessionmaker(
     class_=AsyncSession,
     autocommit=False,
     autoflush=False,
-    expire_on_commit=False
+    expire_on_commit=False,
 )
 
 Base = declarative_base()
+
 
 async def get_postgresql_db() -> AsyncGenerator[AsyncSession, None]:
     async with AsyncPostgresqlSessionLocal() as session:
