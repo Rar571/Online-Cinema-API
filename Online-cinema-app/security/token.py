2706e751a7c8796e3afa6ac625781ac3edff5a55
Ruslan <stadnikruslan168@gmail.com>
2026-06-12 14:38:21 +0300

    Added functions for jwt token creation

--- Online-cinema-app/security/token.py
+++ Online-cinema-app/security/token.py
@@ -1,5 +1,26 @@
+import os
 import secrets
+import jwt
+from datetime import datetime, timezone, timedelta
 
 
 def generate_token(length: int = 32) -> str:
     return secrets.token_urlsafe(length)
+
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM", "HS256")
+
+
+def generate_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp": expire, "type": "access"})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+
+def generate_refresh_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + timedelta(days=7)
+    to_encode.update({"exp": expire, "type": "refresh"})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
