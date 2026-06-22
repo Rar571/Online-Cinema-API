96d60be355059be3cd198a4b703470591baa656c
Ruslan <stadnikruslan168@gmail.com>
2026-06-22 19:58:10 +0300

    feat: add models, crud and endpoints for payments

---
+++ Online-cinema-app/crud/payments.py
@@ -0,0 +1,88 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+import stripe
+
+from models.orders import OrderModel, OrderStatusEnum
+from models.payments import PaymentModel, StatusEnum, PaymentItemModel
+from models.users import UserModel
+from sqlalchemy import select
+import os
+from fastapi import Request, HTTPException, status
+
+
+async def create_checkout_session(
+    order_id: int, db: AsyncSession, current_user: UserModel
+):
+    order_result = await db.execute(select(OrderModel).where(OrderModel.id == order_id))
+    order = order_result.one_or_none()
+    if not order:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Order not found"
+        )
+    if order.user_id != current_user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="No access")
+    if order.status != OrderStatusEnum.PENDING:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="order already paid or canceled",
+        )
+    session = stripe.checkout.Session.create(
+        payment_method_types=["card"],
+        line_items=[
+            {
+                "price_data": {
+                    "currency": "usd",
+                    "product_data": {"name": f"Order #{order.id}"},
+                    "unit_amount": int(order.total_amount * 100),
+                },
+                "quantity": 1,
+            }
+        ],
+        mode="payment",
+        success_url="http://localhost:8000/payments/success?session_id={CHECKOUT_SESSION_ID}",
+        cancel_url="http://localhost:8000/payments/cancel",
+        metadata={"order_id": order.id, "user_id": current_user.id},
+    )
+    return {"checkout_url": session.url}
+
+
+async def stripe_webhook(request: Request, db: AsyncSession):
+    payload = await request.body()
+    sig_header = request.headers.get("stripe-signature")
+
+    event = stripe.Webhook.construct_event(
+        payload, sig_header, os.getenv("STRIPE_WEBHOOK_SECRET")
+    )
+
+    if event["type"] == "checkout.session.completed":
+        session = event["data"]["object"]
+        order_id = session["metadata"]["order_id"]
+        order_result = await db.execute(
+            select(OrderModel).where(OrderModel.id == order_id)
+        )
+        order = order_result.one_or_none()
+        if not order:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND, detail="Order not found"
+            )
+        order.status = OrderStatusEnum.PAID
+        db.add(order)
+        await db.flush()
+        payment = PaymentModel(
+            user_id=session["metadata"]["user_id"],
+            order_id=order_id,
+            amount=session["amount_total"] / 100,
+            status=StatusEnum.SUCCESSFUL,
+            external_payment_id=session.get("payment_intent"),
+        )
+        db.add(payment)
+        await db.flush()
+        for item in order.order_items:
+            payment_item = PaymentItemModel(
+                payment_id=payment.id,
+                order_item_id=item.id,
+                price_at_payment=item.price_at_order,
+            )
+            db.add(payment_item)
+        await db.commit()
+
+    return {"status": "success"}
---
+++ Online-cinema-app/routes/payments.py
@@ -0,0 +1,25 @@
+from fastapi import APIRouter, status, Depends, Request
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from crud.payments import create_checkout_session, stripe_webhook
+from db.session_postgresql import get_db
+from dependencies.users import get_current_user_model
+from models.users import UserModel
+
+payment_router = APIRouter()
+
+
+@payment_router.post("/payments/{order_id}/pay/", status_code=status.HTTP_201_CREATED)
+async def pay_for_order(
+    order_id: int,
+    db: AsyncSession = Depends(get_db),
+    current_user: UserModel = Depends(get_current_user_model),
+):
+    return await create_checkout_session(
+        order_id=order_id, db=db, current_user=current_user
+    )
+
+
+@payment_router.post("/webhook/", status_code=status.HTTP_201_CREATED)
+async def webhook(request: Request, db: AsyncSession = Depends(get_db)):
+    return await stripe_webhook(request=request, db=db)
