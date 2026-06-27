0abd3794b7ba0b3dad829e834f45dac4a54a60e0
Ruslan <stadnikruslan168@gmail.com>
2026-06-27 16:35:36 +0300

    refactor: edit RefreshTokenModel

--- Online-cinema-app/models/users.py
+++ Online-cinema-app/models/users.py
@@ -145,7 +145,7 @@ class RefreshTokenModel(Base):
         ForeignKey("users.id", ondelete="CASCADE"), nullable=False
     )
     user: Mapped["UserModel"] = relationship(
-        "UserModel", back_populates="password_reset_token"
+        "UserModel", back_populates="refresh_token"
     )
     token: Mapped[str] = mapped_column(String(64), unique=True, nullable=False)
     expires_at: Mapped[datetime] = mapped_column(
