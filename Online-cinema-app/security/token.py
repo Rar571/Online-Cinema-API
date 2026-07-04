96a0e5143763b13dfb62dbc31bb3097a3305bdd9
Ruslan <stadnikruslan168@gmail.com>
2026-07-04 22:22:20 +0300

    fix: edit token functions, change algorithm to algorithms attribute

--- Online-cinema-app/security/token.py
+++ Online-cinema-app/security/token.py
@@ -19,7 +19,7 @@ def generate_access_token(data: dict):
     to_encode["sub"] = str(to_encode["sub"])
     expire = datetime.now(timezone.utc) + timedelta(minutes=15)
     to_encode.update({"exp": expire, "type": "access"})
-    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return jwt.encode(to_encode, SECRET_KEY, algorithms=ALGORITHM)
 
 
 def generate_refresh_token(data: dict):
@@ -27,13 +27,13 @@ def generate_refresh_token(data: dict):
     to_encode["sub"] = str(to_encode["sub"])
     expire = datetime.now(timezone.utc) + timedelta(days=7)
     to_encode.update({"exp": expire, "type": "refresh"})
-    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return jwt.encode(to_encode, SECRET_KEY, algorithms=ALGORITHM)
 
 
 def decode_token(token: str) -> int:
     # returns user id or ValueError
     try:
-        payload = jwt.decode(token, SECRET_KEY, algorithm=ALGORITHM)
+        payload = jwt.decode(token, SECRET_KEY, algorithms=ALGORITHM)
         user_id = int(payload.get("sub"))
         return user_id
     except ExpiredSignatureError:
